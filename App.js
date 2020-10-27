5bd6ada397bf995c74aee3abdc80efd1d4236ba8
Rashaun Warner <rashaunwarner@yahoo.com>
2020-10-27 19:22:28 -0400

    socket.io setup working

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+const io = require("socket.io-client");
+
+// Replace this URL with your own, if you want to run the backend locally!
+const SocketEndpoint = "https://6ce4bba4e947.ngrok.io";
+
+
+const App = () => {
+  
+  const [isConnected, setIsConnected] = useState(false)
+  const [userData, setData] = useState(null)
+
+  useEffect(() => {
+    const socket = io(SocketEndpoint, {
+      transports: ["websocket"],
+    });
+    socket.on("connect", () => { setIsConnected(true) })
+    socket.on("ping", (data) => { setData(data) })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>connected: {isConnected ? "true" : "false"}</Text>
+      {userData && <Text>ping response: {userData.data}</Text>}
     </View>
   );
 }
 
+export default App
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
